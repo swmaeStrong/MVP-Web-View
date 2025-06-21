de7b5886ca82a8940b507eef4bb61d39dc0ce20d
younghyun753 <younghyun75398@gmail.com>
2025-06-21 10:44:05 +0900

    feat: url 라우팅 방식 변경

--- src/shared/configs/api/url.ts
+++ src/shared/configs/api/url.ts
@@ -1,11 +1,29 @@
 export const BASEURL =
   process.env.NODE_ENV === 'production'
-    ? process.env.NEXT_PUBLIC_PROD_SERVER_URL
+    ? process.env.SERVER_URL
     : process.env.NEXT_PUBLIC_DEV_SERVER_URL;
 
-export const PORTONE_STORE_ID = process.env.NEXT_PUBLIC_STORE_ID;
+export const PORTONE_STORE_ID =
+  process.env.NODE_ENV === 'production'
+    ? process.env.STORE_ID
+    : process.env.NEXT_PUBLIC_STORE_ID;
+
 export const TOSS_CHANNEL_KEY =
-  process.env.NEXT_PUBLIC_CHANNEL_KEY_TOSS_PAYMENTS;
+  process.env.NODE_ENV === 'production'
+    ? process.env.CHANNEL_KEY_TOSS_PAYMENTS
+    : process.env.NEXT_PUBLIC_CHANNEL_KEY_TOSS_PAYMENTS;
+
+export const KAKAO_PAY_CHANNEL_KEY =
+  process.env.NODE_ENV === 'production'
+    ? process.env.CHANNEL_KEY_KAKAO_PAY
+    : process.env.NEXT_PUBLIC_CHANNEL_KEY_KAKAO_PAY;
+
 export const KAKAOPAY_CHANNEL_KEY =
-  process.env.NEXT_PUBLIC_CHANNEL_KEY_KAKAO_PAY;
-export const APP_URL = process.env.NEXT_PUBLIC_APP_URL;
+  process.env.NODE_ENV === 'production'
+    ? process.env.CHANNEL_KEY_KAKAOPAY
+    : process.env.NEXT_PUBLIC_CHANNEL_KEY_KAKAOPAY;
+
+export const APP_URL =
+  process.env.NODE_ENV === 'production'
+    ? process.env.APP_URL
+    : process.env.NEXT_PUBLIC_APP_URL;
