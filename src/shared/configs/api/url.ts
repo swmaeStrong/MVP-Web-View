2d668ffd5d9eb4d885eeb861603cebc83bb4360c
younghyun753 <younghyun75398@gmail.com>
2025-06-21 10:53:59 +0900

    feat: 토스 페이먼츠 url에서 제거

--- src/shared/configs/api/url.ts
+++ src/shared/configs/api/url.ts
@@ -8,20 +8,10 @@ export const PORTONE_STORE_ID =
     ? process.env.STORE_ID
     : process.env.NEXT_PUBLIC_STORE_ID;
 
-export const TOSS_CHANNEL_KEY =
-  process.env.NODE_ENV === 'production'
-    ? process.env.CHANNEL_KEY_TOSS_PAYMENTS
-    : process.env.NEXT_PUBLIC_CHANNEL_KEY_TOSS_PAYMENTS;
-
 export const KAKAO_PAY_CHANNEL_KEY =
   process.env.NODE_ENV === 'production'
     ? process.env.CHANNEL_KEY_KAKAO_PAY
     : process.env.NEXT_PUBLIC_CHANNEL_KEY_KAKAO_PAY;
-
-export const KAKAOPAY_CHANNEL_KEY =
-  process.env.NODE_ENV === 'production'
-    ? process.env.CHANNEL_KEY_KAKAOPAY
-    : process.env.NEXT_PUBLIC_CHANNEL_KEY_KAKAOPAY;
 
 export const APP_URL =
   process.env.NODE_ENV === 'production'
