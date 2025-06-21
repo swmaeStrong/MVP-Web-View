af56e702c0f851ff00aaa8d6f6106dd6cc0ceeea
younghyun753 <younghyun75398@gmail.com>
2025-06-21 11:59:17 +0900

    feat: url에 NEXT_PUBLIC 추가

--- src/shared/configs/api/url.ts
+++ src/shared/configs/api/url.ts
@@ -1,19 +1,19 @@
 export const BASEURL =
   process.env.NODE_ENV === 'production'
-    ? process.env.SERVER_URL
-    : process.env.NEXT_PUBLIC_DEV_SERVER_URL;
+    ? process.env.NEXT_PUBLIC_SERVER_URL
+    : process.env.NEXT_PUBLIC_SERVER_URL;
 
 export const PORTONE_STORE_ID =
   process.env.NODE_ENV === 'production'
-    ? process.env.STORE_ID
+    ? process.env.NEXT_PUBLIC_STORE_ID
     : process.env.NEXT_PUBLIC_STORE_ID;
 
 export const KAKAO_PAY_CHANNEL_KEY =
   process.env.NODE_ENV === 'production'
-    ? process.env.CHANNEL_KEY_KAKAO_PAY
+    ? process.env.NEXT_PUBLIC_CHANNEL_KEY_KAKAO_PAY
     : process.env.NEXT_PUBLIC_CHANNEL_KEY_KAKAO_PAY;
 
 export const APP_URL =
   process.env.NODE_ENV === 'production'
-    ? process.env.APP_URL
+    ? process.env.NEXT_PUBLIC_APP_URL
     : process.env.NEXT_PUBLIC_APP_URL;
