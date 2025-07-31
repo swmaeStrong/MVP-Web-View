4df0a0b0b980a7a4c0ff79547d6be03f9ab9942b
kyh0726 <younghyun75398@gmail.com>
2025-07-31 18:36:28 +0900

    chore: 그룹 조회 dto 정의

---
+++ src/types/domains/group/group.d.ts
@@ -0,0 +1,28 @@
+declare namespace Group {
+	interface GroupApiResponse {
+		goupId: number;
+		name: string,
+		groupOwner: GroupUserInfo,
+		groupTag: string[] | null,
+		descripotion: string | null,
+	}
+
+	interface GroupUserInfo {
+		userId: string,
+		nickname: string,
+	}
+	
+	interface GroupDetailApiResponse {
+		owner: GroupUserInfo,
+		members: GroupUserInfo[],
+		groupTag: string[] | null,
+		description: string | null,
+		groundRule: string,
+		isPublic: boolean,
+		createdAt: string
+	}
+	
+	
+
+}
+  
